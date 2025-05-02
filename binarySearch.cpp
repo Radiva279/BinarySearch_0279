75653280f48060d3dcbab98604acfec451eb8e48
Radiva279 <radivagalih12@gmail.com>
2025-05-02 09:19:58 +0700

    membuat prosedur input

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -4,3 +4,30 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input()
+{ // prosedur input
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array:";
+        cin >> nPanjang;
+        if (x <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray maksimal 10 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << "=";
+        cin >> element[i];
+    }
+}
